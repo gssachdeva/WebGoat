4f14ff3a7a209369ed1d400b784d5c0be8caad4c
gssachdeva <gurpreets@yahoo.com>
2018-12-28 11:13:06 +0530

    Removed comments

--- scripts/deploy-webgoat.sh
+++ scripts/deploy-webgoat.sh
@@ -3,26 +3,18 @@
 echo "GSS Inside script deploy-webgoat.sh"
 
 export DOCKER_USER=gssachdeva
-export DOCKER_PASS=Sept\$0409
-
-echo "GSS DOCKER_USER = $DOCKER_USER"
-echo "GSS DOCKER_PASS = $DOCKER_PASS"
+export DOCKER_PASS=<PASSWD>
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-#export REPO=webgoat/webgoat-8.0
 export REPO=gssachdeva/webgoat
 
 cd webgoat-server
 ls target/
 
 if [ ! -z "${TRAVIS_TAG}" ]; then
-  echo "GSS Trying to push to Docker hub"
   # If we push a tag to master this will update the LATEST Docker image and tag with the version number
   docker build --build-arg webgoat_version=${TRAVIS_TAG:1} -f Dockerfile -t $REPO:latest -t $REPO:${TRAVIS_TAG} .
-  echo "GSS TRAVIS TAG = $TRAVIS_TAG"
-  echo "GSS REPO = $REPO"
   docker push $REPO
-  echo "GSS After docker push"
 #elif [ ! -z "${TRAVIS_TAG}" ]; then
 #  # Creating a tag build we push it to Docker with that tag
 #  docker build --build-arg webgoat_version=${TRAVIS_TAG:1} -f Dockerfile -t $REPO:${TRAVIS_TAG} -t $REPO:latest .
